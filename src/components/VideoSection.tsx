da6f955ab59aef5170198683529e23262030f745
AnnaKohlbecker <aipskohlbecker@gmail.com>
2025-11-01 18:16:16 +0100

    video

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -1,10 +1,16 @@
-import meVideo from "@/assets/me.mp4";
+import meVideo from "@/assets/me-compressed.mp4";
 import CustomSection from "@/components/CustomSection";
 
 const VideoSection = () => {
   return (
-    <CustomSection>
-      <video width="100%" height="auto" controls className="w-full rounded-2xl">
+    <CustomSection title="INTRODUCTION">
+      <video
+        width="100%"
+        height="auto"
+        controls
+        className="w-full rounded-2xl"
+        preload="metadata"
+      >
         <source src={meVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
